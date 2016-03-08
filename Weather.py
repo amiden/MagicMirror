978e2cf95f568fcb2f69c3e96609e974ba431594
eckama11 <eckhardt.aman@uwlax.edu>
2016-03-07 21:22:18 -0600

    responses
    Finds zipcode, temp, and overall weather conditions for zipcode.

--- Weather.py
+++ Weather.py
@@ -24,19 +24,19 @@ forecast.will_be_sunny_at(tomorrow)  # Always True in Italy, right? ;-)
 # Search for current weather in London (UK)
 observation = owm.weather_at_place('London,uk')
 w = observation.get_weather()
-print(w.get_wind())                      # <Weather - reference time=2013-12-18 09:20, 
-                              # status=Clouds>
+print(w.get_wind())                      # <Weather - reference time=2013-12-18 09:20,
+# status=Clouds>
 
 # Weather details
 w.get_wind()                  # {'speed': 4.6, 'deg': 330}
 w.get_humidity()              # 87
 w.get_temperature('celsius')  # {'temp_max': 10.5, 'temp': 9.7, 'temp_min': 9.0}
 
-# Search current weather observations in the surroundings of 
+# Search current weather observations in the surroundings of
 # lat=22.57W, lon=43.12S (Rio de Janeiro, BR)
 observation_list = owm.weather_around_coords(-22.57, -43.12)
 
-#!/usr/bin/env python 
+#!/usr/bin/env python
 from urllib2 import urlopen
 from contextlib import closing
 import json
@@ -48,9 +48,25 @@ try:
     with closing(urlopen(url)) as response:
         location = json.loads(response.read())
         print(location['zip_code'])
+        #print(location)
         location_zip = location['zip_code']
 except:
     print("Location could not be determined automatically")
+
+# Search for weather at Zipcode
+url1 = 'http://api.openweathermap.org/data/2.5/weather?zip='
+url2 = location_zip
+url3 = ',us&appid=3526f14542bc7277002d2cbb0dee8424'
+urlTemp = url1 + url2 + url3
+try:
+    with closing(urlopen(urlTemp)) as response:
+        data = json.loads(response.read())
+            temp = data['main']['temp']
+                temperature = temp*9/5-459.67
+                print(temperature)
+                print(data['weather'][0]['id'])
+except:
+    print("no kittez")
 
 #kick off the event loop
 root.mainloop()
